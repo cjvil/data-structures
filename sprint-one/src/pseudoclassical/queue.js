9dd40db6352a3f3a6907ab9d5e17a796d62fa3ae
cjvil <christiejvillanueva@gmail.com>
2017-08-24 11:25:39 -0700

    Pass all part 1 tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var trashBin = this.storage[1];
+
+  for (var key in this.storage) {
+    var nextKey = Number(key) + 1;
+    this.storage[key] = this.storage[nextKey];
+  }
+
+  this.count--;
+  delete this.storage[0];
+  return trashBin;
+};
+
+Queue.prototype.size = function() {
+  return this.count < 0 ? 0 : this.count;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style
+  this.count = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+ 
+};
 
+Stack.prototype.pop = function() {
+  var trashBin = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return trashBin;
+
+};
+
+Stack.prototype.size = function() {
+  return this.count < 0 ? 0 : this.count;
+};
