282f2f45786ffe4c018dc161c8006e1d97d66c84
cjvil <christiejvillanueva@gmail.com>
2017-08-25 11:27:07 -0700

    Complete all methods for tree
    All tests pass

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+ // your code here
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,13 +12,34 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+  var parent = this;
+
+  var childSearch = function(parent) {
+    var children = parent.children;
+    if (parent.value === target) {
+      return found = true;
+    }
+    if (children.length > 0) {
+      console.log(parent);    
+      console.log(children);
+      _.each(children, function(child) {
+        childSearch(child);
+      });
+    }
+  };    
+  childSearch(parent, target);
+  return found;
+      
 };
+
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+* Complexity: What is the time complexity of the above functions?
+*/
