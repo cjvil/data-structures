0d1bc4d48efd9ec75675a29a97e801c845140187
cjvil <christiejvillanueva@gmail.com>
2017-08-26 16:28:16 -0700

    Clean up comments in hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,24 +1,7 @@
-/*
-  if we meet the base case where n = 1
-    return 1
-  else
-    divide it into a smaller factorial
-  
-  if (n === 1) {
-    return n
-  }
-  return n * factorial(n - 1)
-
-
-
-*/
-
-
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -28,57 +11,44 @@ HashTable.prototype.insert = function(k, v) {
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, []);
   }
-
-  // save what is currently at index
+  // get what is currently at index
   var output = this._storage.get(index);
-  
-  //and add key inside tuple list
+  // add tuple to current values
   output.push([k, v]);
-  
-  // send modified tuples back into array
+  // send modified tuple array back into storage
   this._storage.set(index, output);
 };
 
-
-// retrieve accepts a key and returns value
 HashTable.prototype.retrieve = function(k) {
-  // get hashed index for key
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //calculate length of tuples at index
   var tuples = this._storage.get(index);
   var found;
-  //return false if length is zero
+
   if (tuples.length === 0) {
-    return found;
+    return undefined;
   }
-  //iterate over each item in the tuple
+
   _.each(tuples, function(tuple) {
     if (tuple[0] === k) {
       found = tuple[1];
     }
   });
-  //determine if the first string is the same string as k
-  //if the first string is the same string, return the tuples second string
-  // if the string is not found within any tuples
-  // return false;
+
   return found;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //store the tuple located at the index
   var tuples = this._storage.get(index);
   var deletionIndex;
-  // iterate over each tupple
+
+  // find index of tuple to delete
   _.each(tuples, function(tuple, tupleIndex) {
     if (tuple[0] === k) {
       deletionIndex = tupleIndex;
     }
   });
 
-  // if the first string in a tuple is the same as string k
-
-  // delete tupple
   tuples.splice(deletionIndex, 1);
   this._storage.set(index, tuples);
 };
