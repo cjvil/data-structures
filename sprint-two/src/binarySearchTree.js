d7162b1e9357b3b48c1021a2534df9cc271df393
cjvil <christiejvillanueva@gmail.com>
2017-08-26 10:42:03 -0700

    Implement insert method for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,42 @@
 var BinarySearchTree = function(value) {
+  var node = {};
+  node.left = null;
+  node.right = null;
+  node.value = value;
+
+  _.extend(node, methods);
+  
+  return node;
 };
+
+var methods = {};
+
+methods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  var parent = this;
+
+  var checkValue = function(parent) {
+    if(parent.value > value && parent.left === null) {
+      parent.left = newNode;
+    } else if(parent.value > value) {
+      checkValue(parent.left);
+    } else if(parent.value < value && parent.right === null) {
+      parent.right = newNode;
+    } else if(parent.value < value) {
+      checkValue(parent.right); 
+    }
+  };
+
+  checkValue(parent);  
+}; 
+
+methods.contains = function(target) {
+
+};
+
+methods.depthFirstLog = function(callback) {
+};
+
 
 
 /*
